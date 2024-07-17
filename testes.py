de72c547bfa21377f22db6e29e7f6ef4da2650f1
Wenderson <wenderson_fagundes@hotmail.com>
2024-07-17 06:45:06 -0300

    first commit

---
+++ testes.py
@@ -0,0 +1,17 @@
+import pytest
+
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+client = TestClient(app)
+
+def test_ola_mundo():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_ola_mundo_json():
+    response = client.get("/")
+    assert response.json() == {"Olá": "Mundo"}
